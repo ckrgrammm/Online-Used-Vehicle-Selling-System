2bc19fa1caedc6f36a3907d5a163d9506e1c6395
LAPTOP-LKCOQMQM\user <limeh-wm20@student.tarc.edu.my>
2023-03-05 16:18:51 +0800

    design admin table

--- resources/views/admin/all-customer.blade.php
+++ resources/views/admin/all-customer.blade.php
@@ -1,7 +1,37 @@
 @extends('admin/master')
 @section('content')
-<table id="example" class="table table-striped" style="width:100%">
+<style>
+
+.avatar {
+  width: 2.75rem;
+  height: 2.75rem;
+  line-height: 3rem;
+  border-radius: 50%;
+  display: inline-block;
+  background: transparent;
+  position: relative;
+  text-align: center;
+  color: #868e96;
+  font-weight: 700;
+  vertical-align: bottom;
+  font-size: 1rem;
+  margin-right: 1rem;
+  background-size: cover;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+
+</style>
+
+<table id="example" class="table table-hover" style="width:100%">
     <thead>
+        <tr>
+            <th>
+                <a href="" class="btn btn-primary" title="Add">Add<i class="mdi mdi-plus-circle-outline"></i></a>
+            </th>
+        </tr>
         <tr>
             <th>Name</th>
             <th>Position</th>
@@ -9,16 +39,30 @@
             <th>Age</th>
             <th>Start date</th>
             <th>Salary</th>
+            <th>Action</th>
         </tr>
     </thead>
     <tbody>
         <tr>
-            <td>Tiger Nixon</td>
+            <td>
+                <div class="d-flex align-items-center">
+                  <div class="avatar mr-3" style="background-image: url({{asset('user/img/profile/59901786640343b8c1a77.png')}})"></div>
+
+                  <div class="">
+                    <p class="font-weight-bold mb-0">Ethan Black</p>
+                    <p class="text-muted mb-0">ethan-black@example.com</p>
+                  </div>
+                </div>
+            </td>
             <td>System Architect</td>
             <td>Edinburgh</td>
             <td>61</td>
             <td>2011-04-25</td>
             <td>$320,800</td>
+            <td> 
+                <a href="" class="btn btn-success" title="Edit"><i class="mdi mdi-square-edit-outline"></i></a>
+                <a href="" class="btn btn-danger" title="Delete"><i class="mdi mdi-delete-outline"></i></a>
+            </td>
         </tr>
         <tr>
             <td>Garrett Winters</td>
@@ -27,134 +71,10 @@
             <td>63</td>
             <td>2011-07-25</td>
             <td>$170,750</td>
-        </tr>
-        <tr>
-            <td>Ashton Cox</td>
-            <td>Junior Technical Author</td>
-            <td>San Francisco</td>
-            <td>66</td>
-            <td>2009-01-12</td>
-            <td>$86,000</td>
-        </tr>
-        <tr>
-            <td>Cedric Kelly</td>
-            <td>Senior Javascript Developer</td>
-            <td>Edinburgh</td>
-            <td>22</td>
-            <td>2012-03-29</td>
-            <td>$433,060</td>
-        </tr>
-        <tr>
-            <td>Caesar Vance</td>
-            <td>Pre-Sales Support</td>
-            <td>New York</td>
-            <td>21</td>
-            <td>2011-12-12</td>
-            <td>$106,450</td>
-        </tr>
-        <tr>
-            <td>Doris Wilder</td>
-            <td>Sales Assistant</td>
-            <td>Sydney</td>
-            <td>23</td>
-            <td>2010-09-20</td>
-            <td>$85,600</td>
-        </tr>
-        <tr>
-            <td>Angelica Ramos</td>
-            <td>Chief Executive Officer (CEO)</td>
-            <td>London</td>
-            <td>47</td>
-            <td>2009-10-09</td>
-            <td>$1,200,000</td>
-        </tr>
-        <tr>
-            <td>Gavin Joyce</td>
-            <td>Developer</td>
-            <td>Edinburgh</td>
-            <td>42</td>
-            <td>2010-12-22</td>
-            <td>$92,575</td>
-        </tr>
-        <tr>
-            <td>Jennifer Chang</td>
-            <td>Regional Director</td>
-            <td>Singapore</td>
-            <td>28</td>
-            <td>2010-11-14</td>
-            <td>$357,650</td>
-        </tr>
-        <tr>
-            <td>Brenden Wagner</td>
-            <td>Software Engineer</td>
-            <td>San Francisco</td>
-            <td>28</td>
-            <td>2011-06-07</td>
-            <td>$206,850</td>
-        </tr>
-        <tr>
-            <td>Fiona Green</td>
-            <td>Chief Operating Officer (COO)</td>
-            <td>San Francisco</td>
-            <td>48</td>
-            <td>2010-03-11</td>
-            <td>$850,000</td>
-        </tr>
-        <tr>
-            <td>Shou Itou</td>
-            <td>Regional Marketing</td>
-            <td>Tokyo</td>
-            <td>20</td>
-            <td>2011-08-14</td>
-            <td>$163,000</td>
-        </tr>
-        <tr>
-            <td>Timothy Mooney</td>
-            <td>Office Manager</td>
-            <td>London</td>
-            <td>37</td>
-            <td>2008-12-11</td>
-            <td>$136,200</td>
-        </tr>
-        <tr>
-            <td>Jackson Bradshaw</td>
-            <td>Director</td>
-            <td>New York</td>
-            <td>65</td>
-            <td>2008-09-26</td>
-            <td>$645,750</td>
-        </tr>
-        <tr>
-            <td>Olivia Liang</td>
-            <td>Support Engineer</td>
-            <td>Singapore</td>
-            <td>64</td>
-            <td>2011-02-03</td>
-            <td>$234,500</td>
-        </tr>
-        <tr>
-            <td>Bruno Nash</td>
-            <td>Software Engineer</td>
-            <td>London</td>
-            <td>38</td>
-            <td>2011-05-03</td>
-            <td>$163,500</td>
-        </tr>
-        <tr>
-            <td>Sakura Yamamoto</td>
-            <td>Support Engineer</td>
-            <td>Tokyo</td>
-            <td>37</td>
-            <td>2009-08-19</td>
-            <td>$139,575</td>
-        </tr>
-        <tr>
-            <td>Thor Walton</td>
-            <td>Developer</td>
-            <td>New York</td>
-            <td>61</td>
-            <td>2013-08-11</td>
-            <td>$98,540</td>
+            <td> 
+                <a href="" class="btn btn-success" title="Edit"><i class="mdi mdi-square-edit-outline"></i></a>
+                <a href="" class="btn btn-danger" title="Delete"><i class="mdi mdi-delete-outline"></i></a>
+            </td>
         </tr>
     </tbody>
 </table>
--- resources/views/admin/master.blade.php
+++ resources/views/admin/master.blade.php
@@ -19,6 +19,89 @@
     <script src="{{asset('admin/assets/vendors/js/vendor.bundle.base.js')}}"></script>
     <script src="https://cdn.datatables.net/1.13.3/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/1.13.3/js/dataTables.bootstrap5.min.js"></script>
+
+    <style>
+        .table {
+        border-spacing: 0 0.85rem !important;
+        }
+
+        .table td,
+        .table th {
+        vertical-align: middle;
+        margin-bottom: 10px;
+        border: none;
+        }
+
+        .table thead tr,
+        .table thead th {
+        border: none;
+        font-size: 12px;
+        letter-spacing: 1px;
+        text-transform: uppercase;
+        background: transparent;
+        }
+
+        .table td {
+        background: #fff;
+        }
+
+        .table td:first-child {
+        border-top-left-radius: 10px;
+        border-bottom-left-radius: 10px;
+        }
+
+        .table td:last-child {
+        border-top-right-radius: 10px;
+        border-bottom-right-radius: 10px;
+        }
+
+        table.dataTable.dtr-inline.collapsed
+        > tbody
+        > tr[role="row"]
+        > td:first-child:before,
+        table.dataTable.dtr-inline.collapsed
+        > tbody
+        > tr[role="row"]
+        > th:first-child:before {
+        top: 28px;
+        left: 14px;
+        border: none;
+        box-shadow: none;
+        }
+
+        table.dataTable.dtr-inline.collapsed > tbody > tr[role="row"] > td:first-child,
+        table.dataTable.dtr-inline.collapsed > tbody > tr[role="row"] > th:first-child {
+        padding-left: 48px;
+        }
+
+        table.dataTable > tbody > tr.child ul.dtr-details {
+        width: 100%;
+        }
+
+        table.dataTable > tbody > tr.child span.dtr-title {
+        min-width: 50%;
+        }
+
+        table.dataTable.dtr-inline.collapsed > tbody > tr > td.child,
+        table.dataTable.dtr-inline.collapsed > tbody > tr > th.child,
+        table.dataTable.dtr-inline.collapsed > tbody > tr > td.dataTables_empty {
+        padding: 0.75rem 1rem 0.125rem;
+        }
+
+        div.dataTables_wrapper div.dataTables_length label,
+        div.dataTables_wrapper div.dataTables_filter label {
+        margin-bottom: 0;
+        }
+
+        .table a:hover,
+        .table a:focus {
+        text-decoration: none;
+        }
+
+        table.dataTable {
+        margin-top: 12px !important;
+        }
+    </style>
 </head>
 <body>
     <div class="container-scroller">
